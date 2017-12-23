dc06cc6ad06d5be8e822d5c7f170cb44d8cef077
Hijiri K <hijiri.k.emk.p@gmail.com>
2017-12-24 05:26:17 +0900

    third change config.action_cable.allowed_request_origins for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,11 +43,7 @@ Rails.application.configure do
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
-<<<<<<< HEAD
  config.action_cable.allowed_request_origins = ['/https?:\/\/.*/'] 
-=======
- config.action_cable.allowed_request_origins = ['https://actioncable-test-app.herokuapp.com'] 
->>>>>>> 67c6fa397a076209ec48ce16713d634da80c287c
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
