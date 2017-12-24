c534c94ae8f67e0c79be6a04caa9623c4bfe566a
Hijiri K <hijiri.k.emk.p@gmail.com>
2017-12-24 15:23:11 +0900

    error fix

--- app/channels/room_channel.rb
+++ app/channels/room_channel.rb
@@ -8,7 +8,8 @@ class RoomChannel < ApplicationCable::Channel
   end
 
   def speak(data)
-    ActionCable.server.broadcast 'room_channel', message: data['message']
-    # Message.create! content: data['message']
+    # ActionCable.server.broadcast 'room_channel', message: data['message']
+    Message.create! content: data['message']
+    Alart data['message']
   end
 end
